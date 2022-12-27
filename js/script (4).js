50f8856dbc05245d6f861e29f012d21a8c024cf2
maxaniskin <109670058+maxaniskin@users.noreply.github.com>
2022-12-27 09:48:44 +0300

    Add files via upload

---
+++ js/script (4).js
@@ -0,0 +1,11 @@
+
+var x = document.getElementById("containt");
+
+x.addEventListener("click", myFunction);
+
+function myFunction() {
+  var element = document.getElementById("nav1");
+  element.classList.toggle("opened");
+  
+}
+
---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+var x = document.getElementById("containt");
+
+x.addEventListener("click", myFunction);
+
+function myFunction() {
+  var element = document.getElementById("nav1");
+  element.classList.toggle("opened");
+  
+}
+
+var x = document.getElementById("containt1");
+
+x.addEventListener("click", myFunction);
+
+function myFunction() {
+  var element = document.getElementById("nav1");
+  element.classList.toggle("opened");
+  
+}
+var x = document.getElementById("containt3");
+
+x.addEventListener("click", myFunction);
+
+function myFunction() {
+  var element = document.getElementById("nav1");
+  element.classList.toggle("opened");
+  
+}
+
